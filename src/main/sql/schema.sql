2de2bb8b3ce2db452505f8f06636a27dead76972
lw1000 <1578318880@qq.com>
2018-09-24 15:18:27 +0800

    新增储存过程

--- src/main/sql/schema.sql
+++ src/main/sql/schema.sql
@@ -6,7 +6,7 @@ USE seckill;
 --创建秒杀库存表
 CREATE TABLE seckill(
   seckill_id BIGINT NOT NULL AUTO_INCREMENT COMMENT '商品库存id',
- `name` VARCHAR(120) NOT NULL COMMENT '商品名称',
+  `name` VARCHAR(120) NOT NULL COMMENT '商品名称',
   number int NOT NULL COMMENT '库存数量',
   start_time  TIMESTAMP NOT NULL COMMENT '秒杀开始时间',
   end_time  datetime NOT NULL COMMENT '秒杀结束时间',
@@ -15,7 +15,7 @@ CREATE TABLE seckill(
   key idx_start_time(start_time),
   key idx_end_time(end_time),
   key idx_create_time(create_time)
- )ENGINE = InnoDB
+)ENGINE = InnoDB
   AUTO_INCREMENT=1000
   DEFAULT CHARSET=utf8
   COMMENT='秒杀库存表';
@@ -32,16 +32,16 @@ VALUES
 --秒杀成功明细表
 --用户登陆认证的相关信息
 CREATE TABLE success_kill(
-    seckill_id BIGINT NOT NULL AUTO_INCREMENT COMMENT '秒杀商品id',
-  	user_phone  BIGINT NOT NULL COMMENT '用户手机号',
-    state  TINYINT NOT NULL DEFAULT-1 COMMENT '状态标识，-1无效，0成功，1已付款',
-  	create_time TIMESTAMP NOT NULL COMMENT '创建时间',
-	  PRIMARY KEY (seckill_id,user_phone)
+  seckill_id BIGINT NOT NULL AUTO_INCREMENT COMMENT '秒杀商品id',
+  user_phone  BIGINT NOT NULL COMMENT '用户手机号',
+  state  TINYINT NOT NULL DEFAULT-1 COMMENT '状态标识，-1无效，0成功，1已付款',
+  create_time TIMESTAMP NOT NULL COMMENT '创建时间',
+  PRIMARY KEY (seckill_id,user_phone)
 )
-    ENGINE=InnoDB
-    AUTO_INCREMENT=1000
-    DEFAULT CHARSET=utf8
-    COMMENT '秒杀成功明细表';
+  ENGINE=InnoDB
+  AUTO_INCREMENT=1000
+  DEFAULT CHARSET=utf8
+  COMMENT '秒杀成功明细表';
 
 
 
